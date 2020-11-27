27846feb6e837f471ffa9dc6f9b51baeab6cbea3
Frantz Hedrich <frantzhed@gmail.com>
2020-11-27 11:28:35 +0100

    fixed filters front

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,5 +48,6 @@ end
   Booking.create!(category: category.sample, price: 50, group_session: Faker::Boolean.boolean, user_id: User.last.id, start_date: Faker::Date.between(from:30.days.ago, to: Date.today), end_date:Faker::Date.between(from:Date.today, to: 30.days.from_now)  )
   puts "Created one booking"
 
-  Review.create!(content: "Very Good experience", rate: rand(0..5), user_id: User.last.id, booking_id: Booking.last.id)
+  Review.create!(content: "We had a wonderful day skiing with the best intructor!", rate: 5, user_id: User.last.id, booking_id: Booking.last.id)
+  Review.create!(content: "We had a great day skiing with the best intructor!", rate: 3, user_id: User.last.id, booking_id: Booking.last.id)
 end
