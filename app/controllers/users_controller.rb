f8c4a95acacb28bb9ef17fe49021631493b0e19b
Nicolas le Bechec de Redon <nicolaconilebechec@gmail.com>
2020-11-27 12:38:45 +0100

    avec lomig

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,18 @@ class UsersController < ApplicationController
 
   def index
     @users = User.instructor
-    if params.dig(:user, :resort).present?
-      @users = @users.where("ski_resort ILIKE ?", "%#{params[:user][:resort]}%")
+    if params.dig(:user, :ski_resort).present?
+      @users = @users.where("ski_resort ILIKE ?", "%#{params[:user][:ski_resort]}%")
     end
     if params.dig(:user, :category).present?
       @users = @users.joins(:categories).where(categories: {name: params[:user][:category]})
     end
     if params[:user][:technical_skill].present?
-      @users = @users.where("technical_skill ILIKE ?", "%#{params[:user][:technical_skill]}%")
+      @users = @users.where(technical_skill: params[:user][:technical_skill])
     end
-    
-    
-    
-
+    if params[:user][:languages].present?
+      @users = @users.where("? = ANY(languages)", "#{params[:user][:languages]}")
+    end
   end
 
   def show
@@ -28,5 +27,4 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
-
 end
