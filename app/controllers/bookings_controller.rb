ea373cb0c947a181868bda20806928e31972cc11
Nicolas le Bechec de Redon <nicolaconilebechec@gmail.com>
2020-12-03 16:35:36 +0100

    jeudisoir

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,10 +5,10 @@ class BookingsController < ApplicationController
 
   end
 
-def show
-  @booking = Booking.find(params[:id])
+  def show
+    @booking = Booking.find(params[:id])
 
-end
+  end
 
   def new
     @user = User.find(params[:user_id])
@@ -20,7 +20,7 @@ end
     @booking = Booking.new(booking_params)
     @booking.amount = 45
     @booking.instructor = @instructor
-    @booking.user_sku = @instructor.first_name[0..3] + "-"+ @instructor.last_name[0..3]
+    @booking.user_sku = @instructor.first_name[0..3] + "-" + @instructor.last_name[0..3]
     if @booking.save
       @user_booking = UserBooking.create(
         user: current_user,
@@ -28,34 +28,29 @@ end
         booking: @booking,
         participants_number: booking_params[:participants_number]
       )
-
       redirect_to dashboard_path
     else
-
-      render "users/show", #locals: {id: @booking.instructor}stripe?
-
+      render "users/show" #locals: {id: @booking.instructor}stripe?
     end
   end
 
   def booking_accepted
-
     booking = Booking.find(params[:booking_id])
     session = Stripe::Checkout::Session.create(
-                 payment_method_types: ['card'],
-                 line_items: [{
-                   name: booking.user_sku,
-                   images: [booking.instructor.photo],
-                   amount: booking.amount_cents,
-                   currency: 'eur',
-                   quantity: 1
-                 }],
-                 success_url: dashboard_url,
-                 cancel_url: dashboard_url
-               )
+      payment_method_types: ['card'],
+        line_items: [{
+         name: booking.user_sku,
+         images: [booking.instructor.photo],
+         amount: booking.amount_cents,
+         currency: 'eur',
+         quantity: 1
+       }],
+       success_url: dashboard_url,
+       cancel_url: dashboard_url
+     )
 
-             booking.update(checkout_session_id: session.id,status: "Paid")
-            redirect_to dashboard_path
-
+      booking.update(checkout_session_id: session.id, status: "Paid")
+      redirect_to dashboard_path
   end
 
   def edit
@@ -78,7 +73,6 @@ end
     Booking.find(params[:id]).update(status: params[:status])
   end
 
-
   private
 
   def booking_params
@@ -89,4 +83,3 @@ end
     @booking = Booking.find(params[:id])
   end
 end
-
