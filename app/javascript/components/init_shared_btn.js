84d12326e679b186b81aed5755e99ce7a2d0d1a3
Cecile Dezy <cecile.dezy@gmail.com>
2020-12-03 15:20:37 +0100

    home index show

--- app/javascript/components/init_shared_btn.js
+++ app/javascript/components/init_shared_btn.js
@@ -1,7 +1,7 @@
 const initSharedBtn = () => {
   if (document.querySelector("#Shared")) {
     const btn = document.querySelector("#Shared");
-    const element = document.querySelector("#myTabContent");
+    const element = document.querySelector(".mountain-index");
     btn.addEventListener("click", (e) => {
       element.scrollIntoView({ behavior: "smooth", block: "end" });
     });
